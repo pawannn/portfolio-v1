2b1091b93bd2ddff8a6b8ef9cf39b6a1a6137f3f
Pawan Kalyan <pawankalyan1614@gmail.com>
2023-01-14 22:19:17 +0530

    netlify mail

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -5,21 +5,21 @@ const ContactForm = () => {
     return (
             <div className="container">
                 <form class="form" method="POST" netlify>
-                <div>
-                    <p>Name</p>
-                    <input type = "text" placeholder="Your Lovely Name"/>
-                </div>
-                <div>
-                    <p>Email</p>
-                    <input type = "email" placeholder="Your Email ID"/>
-                </div>
-                <div>
-                    <p>Message</p>
-                    <textarea style = {{ resize : "none" }}placeholder="Lets Start the Conversation"></textarea>
-                </div>
-                <div>
-                    <button>Submit</button>
-                </div>
+                    <div>
+                        <p>Name</p>
+                        <input type = "text" placeholder="Your Lovely Name" name = "name"/>
+                    </div>
+                    <div>
+                        <p>Email</p>
+                        <input type = "email" placeholder="Your Email ID" name = "email"/>
+                    </div>
+                    <div>
+                        <p>Message</p>
+                        <textarea style = {{ resize : "none" }}placeholder="Lets Start the Conversation" name = "description"></textarea>
+                    </div>
+                    <div>
+                        <button type="submit">Submit</button>
+                    </div>
                 </form>
             </div>  
     )
