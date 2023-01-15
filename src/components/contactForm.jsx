717aeb95f1a81d0edf5bf98dd6440ef851be3b89
Pawan Kalyan <pawankalyan1614@gmail.com>
2023-01-15 20:00:28 +0530

    netlify mail problem

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -4,7 +4,7 @@ import '../style/contactForm.css'
 const ContactForm = () => {
     return (
             <div className="container">
-                <form name="contactform" method="post" data-netlify="true">
+                <form name="contactform" action="post" data-netlify="true">
                     <div>
                         <p>Name</p>
                         <input type = "text" name = "name" placeholder="Your Lovely Name" />
